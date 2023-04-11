2ca5ee03bf5887df615b5a881423fbece4b2ef2b
shonya3 <poeshonya3@gmail.com>
2023-04-11 20:29:47 +0300

    refactor: early return on error

--- src/lib.ts
+++ src/lib.ts
@@ -34,16 +34,7 @@ export const command = <T extends keyof CommandList>(
 	return invoke(cmd, args) as CommandList[T]['returnType'];
 };
 
-const calcRecordWeight = (record: CardRecord, allStackSize: number): number => record.stackSize / allStackSize;
-const calcRecordRealWeight = (
-	record: CardRecord,
-	realStackedSummaryWeight: number,
-	condenseFactor: number,
-	allStackSize: number
-): number => (realStackedSummaryWeight * calcRecordWeight(record, allStackSize)) ** (1 / condenseFactor);
-
 export const createContents = async (file: File): Promise<Contents> => {
-	let valid = false;
 	let error = null;
 	let text = await file.text();
 	let href = '';
@@ -58,35 +49,21 @@ export const createContents = async (file: File): Promise<Contents> => {
 		notCards = data.notCards;
 		fixedNames = data.fixedNames;
 		href = createCSVLink(text);
-		valid = true;
 	} catch (err) {
 		error = err as string;
+		console.log(err);
 	}
 
-	// const allStackSize = records.reduce((summ, { stackSize }) => (summ += stackSize), 0);
-	// const REAL_STACKED_RAIN_OF_CHAOS_WEIGHT = 2452.65513;
-	// const CONDENSE_FACTOR = 2 / 3;
-	// const rainOfChaos = records.find(({ name }) => name === 'Rain of Chaos');
-	// if (!rainOfChaos) throw new Error('No Rain of Chaos card');
-	// const weight = rainOfChaos.stackSize / allStackSize;
-	// const realStackedSummaryWeight = REAL_STACKED_RAIN_OF_CHAOS_WEIGHT / weight;
-	// const weightedRecords: WeightedCardRecord[] = records.map(record => {
-	// 	const realWeight = calcRecordRealWeight(record, realStackedSummaryWeight, CONDENSE_FACTOR, allStackSize);
-	// 	return {
-	// 		...record,
-	// 		realWeight,
-	// 	};
-	// });
-
 	let weightedRecords: WeightedCardRecord[] = [];
-	try {
-		const { csv, records: weighted } = await command('weight_records_to_csv', { records });
-		text = csv;
-		href = createCSVLink(text);
-		valid = true;
-		weightedRecords = weighted;
-	} catch (err) {
-		error = err as string;
+	if (!error) {
+		try {
+			const { csv, records: weighted } = await command('weight_records_to_csv', { records });
+			text = csv;
+			href = createCSVLink(text);
+			weightedRecords = weighted;
+		} catch (err) {
+			error = err as string;
+		}
 	}
 
 	const fileContent: FileContents = {
@@ -97,7 +74,7 @@ export const createContents = async (file: File): Promise<Contents> => {
 	const contents: Contents = {
 		id: crypto.randomUUID(),
 		fileContent,
-		valid,
+		valid: !Boolean(error),
 		error,
 		selected: false,
 		price: 0,
