dc791c6d49b5a8e67f10e2de0b8d1dbc069205e2
shonya3 <poeshonya3@gmail.com>
2023-04-26 16:00:17 +0300

    rm boilerplate

--- src-tauri/lib/src/lib.rs
+++ src-tauri/lib/src/lib.rs
@@ -3,18 +3,3 @@ pub mod dev;
 pub mod file_card_data;
 pub mod prices;
 pub mod starter_map;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
