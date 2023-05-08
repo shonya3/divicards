ccfca2af69089177e69c161c2b93fcaa5fcfe04c
shonya3 <poeshonya3@gmail.com>
2023-05-08 08:31:11 +0300

    improve Commands interface

--- src/command.ts
+++ src/command.ts
@@ -5,36 +5,25 @@ export type CommandOptions = {
 	log: boolean;
 };
 
+export interface Commands {
+	sample: (args: { csv: string }) => DivinationCardsSample;
+	chaos: (args: { sample: DivinationCardsSample; min: number }) => number;
+	merge: (args: { samples: DivinationCardsSample[] }) => DivinationCardsSample;
+}
+
 export const command = async <Cmd extends keyof Commands>(
 	cmd: Cmd,
-	args: Commands[Cmd]['args'],
+	args: Parameters<Commands[Cmd]>[0],
 	options?: CommandOptions
-): Promise<Commands[Cmd]['returns']> => {
+): Promise<ReturnType<Commands[Cmd]>> => {
 	if (import.meta.env.DEV && !options) {
 		options = { log: true };
 	}
 
 	if (options?.log) {
 		const t0 = performance.now();
-		const res = (await invoke(cmd, args)) as Commands[Cmd]['returns'];
+		const res = (await invoke(cmd, args)) as ReturnType<Commands[Cmd]>;
 		console.log(`${cmd}: ${new Intl.NumberFormat().format(performance.now() - t0)}ms`);
 		return res;
-	} else return invoke(cmd, args) as Promise<Commands[Cmd]['returns']>;
+	} else return invoke(cmd, args) as Promise<ReturnType<Commands[Cmd]>>;
 };
-
-export interface Commands {
-	sample: {
-		args: { csv: string };
-		returns: DivinationCardsSample;
-	};
-
-	chaos: {
-		args: { sample: DivinationCardsSample; min: number };
-		returns: number;
-	};
-
-	merge: {
-		args: { samples: DivinationCardsSample[] };
-		returns: DivinationCardsSample;
-	};
-}
