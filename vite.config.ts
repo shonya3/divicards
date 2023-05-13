15707515ef96978606c84a884a7606c00f4eddf9
shonya3 <poeshonya3@gmail.com>
2023-05-13 15:00:34 +0300

    add devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+	plugins: [vue(), VueDevTools()],
+});
