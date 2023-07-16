be4a079dd4531401d097d6b05a9efea977245ca0
shonya3 <poeshonya3@gmail.com>
2023-07-16 13:37:38 +0300

    remove TabBadeGroup.vue

--- src/components/wc/stashes/dummy.ts
+++ src/components/wc/stashes/dummy.ts
@@ -1,4 +1,4 @@
 import json from './tabBadgeGroupProps.json' assert { type: 'json' };
-import { League } from '../../types';
-import { StatefulStashTab } from '../../stores/stash';
+import { League } from '../../../types';
+import { StatefulStashTab } from '../../../stores/stash';
 export const tabBadgeProps = json as { league: League; stashes: StatefulStashTab[] };
