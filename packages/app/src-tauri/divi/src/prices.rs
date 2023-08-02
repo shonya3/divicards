ee4fcfaf168375a37cc56cbef3b2a6f5efaff686
shonya3 <poeshonya3@gmail.com>
2023-08-02 13:35:29 +0300

    update fetch prices method

--- packages/app/src-tauri/divi/src/prices.rs
+++ packages/app/src-tauri/divi/src/prices.rs
@@ -6,24 +6,34 @@ use crate::{
 };
 use serde_big_array::BigArray;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct DivinationCardPrice {
     pub name: String,
     #[serde(alias = "chaosValue")]
     pub price: Option<f32>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[serde(transparent)]
 pub struct Prices(#[serde(with = "BigArray")] pub [DivinationCardPrice; CARDS_N]);
 impl Prices {
     pub async fn fetch(league: &TradeLeague) -> Result<Prices, reqwest::Error> {
-        Ok(Prices(DivinationCardPrice::fetch(league).await?))
+        #[derive(Deserialize, Debug, Serialize)]
+        struct PriceData {
+            lines: Vec<DivinationCardPrice>,
+        }
+
+        let client = reqwest::Client::new();
+        let url = format!("https://poe.ninja/api/data/itemoverview?league={league}&type=DivinationCard&language=en");
+        let json = client.get(url).send().await?.text().await?;
+        let data = serde_json::from_str::<PriceData>(&json).unwrap();
+        Ok(Prices::from(data.lines))
     }
 }
+
 impl Default for Prices {
     fn default() -> Self {
-        CARDS.into()
+        Prices::from(CARDS)
     }
 }
 
@@ -42,38 +52,14 @@ impl From<[&'static str; CARDS_N]> for Prices {
     }
 }
 
-impl DivinationCardPrice {
-    pub async fn fetch(league: &TradeLeague) -> Result<[Self; CARDS_N], reqwest::Error> {
-        #[derive(Deserialize, Debug, Serialize)]
-        struct PriceData {
-            lines: Vec<DivinationCardPrice>,
+impl From<Vec<DivinationCardPrice>> for Prices {
+    fn from(value: Vec<DivinationCardPrice>) -> Self {
+        let mut prices = Prices::default();
+        for card in prices.0.iter_mut() {
+            if let Some(found) = value.iter().find(|c| card.name == c.name) {
+                card.price = found.price
+            }
         }
-
-        let client = reqwest::Client::new();
-        let url = format!(
-            "https://poe.ninja/api/data/itemoverview?league={league}&type=DivinationCard&language=en",
-        );
-        let json = client.get(url).send().await?.text().await?;
-
-        let price_data: PriceData = serde_json::from_str(&json).unwrap();
-        let prices = price_data.lines;
-
-        let prices_arr: [DivinationCardPrice; CARDS_N] = CARDS
-            .into_iter()
-            .map(|card| {
-                let price = prices
-                    .iter()
-                    .find(|div_card_price| div_card_price.name == card)
-                    .and_then(|v| v.price);
-                DivinationCardPrice {
-                    name: card.to_string(),
-                    price,
-                }
-            })
-            .collect::<Vec<Self>>()
-            .try_into()
-            .unwrap();
-
-        Ok(prices_arr)
+        prices
     }
 }
