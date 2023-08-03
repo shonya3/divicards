9a29839d3f14bf0ae51f62d694015d52c89186df
shonya3 <poeshonya3@gmail.com>
2023-08-03 13:18:33 +0300

    wc:div-table:highlight the hovered row

--- packages/wc/src/wc/div-table/div-table.ts
+++ packages/wc/src/wc/div-table/div-table.ts
@@ -294,6 +294,17 @@ function styles() {
 		tr {
 			display: grid;
 			grid-template-columns: 0.5fr 1.2fr 3fr 1fr 1fr 1fr;
+
+			&:hover {
+				outline: 1px black solid;
+				box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset,
+					rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px 0px,
+					rgba(0, 0, 0, 0.09) 0px 4px 2px 0px, rgba(0, 0, 0, 0.09) 0px 8px 4px 0px,
+					rgba(0, 0, 0, 0.09) 0px 16px 8px 0px, rgba(0, 0, 0, 0.09) 0px 32px 16px 0px;
+				filter: > .name-row {
+
+				}
+			}
 		}
 
 		.ch-3 {
