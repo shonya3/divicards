91bf79f5f3a3bcd32a88622263279134110a5a78
shonya3 <poeshonya3@gmail.com>
2023-08-25 20:03:45 +0300

    update mock data

--- packages/wc/src/wc/stashes/data.ts
+++ packages/wc/src/wc/stashes/data.ts
@@ -1,18 +1,16 @@
-import { DivinationCardsSample, League, Result } from '@divicards/shared/types';
+import { DivinationCardsSample, League } from '@divicards/shared/types';
 import { StashTab } from '@divicards/shared/poe.types';
 import { IStashLoader } from '@divicards/shared/StashLoader';
 
-import stashData from './stash.json' assert { type: 'json' };
 import stashesData from './stashes.json' assert { type: 'json' };
-import resultSample from './resultSample.json' assert { type: 'json' };
+import sampleData from './sample.json' assert { type: 'json' };
 
-export const stash: StashTab = stashData as unknown as StashTab;
 export const stashes: StashTab[] = stashesData;
 export const league: League = 'Standard';
-export const sample: Result<DivinationCardsSample> = resultSample as Result<DivinationCardsSample>;
+export const sample: DivinationCardsSample = sampleData;
 
 export class MockStashLoader implements IStashLoader {
-	sampleFromTab(_tabId: string, _league: League): Promise<Result<DivinationCardsSample>> {
+	sampleFromTab(_tabId: string, _league: League): Promise<DivinationCardsSample> {
 		return new Promise(r => r(sample));
 	}
 	tabs(_league: League): Promise<StashTab[]> {
