f30116f1cb6d7fafdbf9abb5f36aae823b2365ca
shonya3 <poeshonya3@gmail.com>
2023-08-25 08:19:43 +0300

    divi: add tests

--- divi/src/lib.rs
+++ divi/src/lib.rs
@@ -24,22 +24,66 @@ pub trait IsCard {
     fn is_legacy_card(&self) -> bool;
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use std::fs;
+#[cfg(test)]
+mod weight_tests {
+    use std::fs;
 
-//     use crate::{
-//         prices::Prices,
-//         sample::{DivinationCardsSample, SampleData},
-//     };
+    use crate::{
+        consts::{CONDENSE_FACTOR, RAIN_OF_CHAOS_WEIGHT},
+        prices::Prices,
+        sample::{DivinationCardsSample, SampleData},
+    };
 
-//     #[test]
-//     fn fix_typos() {
-//         let prices: Prices =
-//             serde_json::from_str(&fs::read_to_string(&"Crucible-prices.json").unwrap()).unwrap();
+    #[test]
+    fn calc_real_rain_of_chaos_weight() {
+        let sample = DivinationCardsSample::create(
+            SampleData::Csv(String::from("name,amount\rRain of Chaos,1")),
+            Some(Prices::default()),
+        )
+        .unwrap();
 
-//         let sample_data = SampleData::Csv(fs::read_to_string("example-3.csv").unwrap());
-//         let sample = DivinationCardsSample::create(sample_data, Some(prices)).unwrap();
-//         assert_eq!(sample.fixed_names.len(), 26);
-//     }
-// }
+        let card = sample.cards.get_card("Rain of Chaos");
+        assert_eq!(
+            //121465.99-ish
+            RAIN_OF_CHAOS_WEIGHT.powf(1.0 / CONDENSE_FACTOR),
+            card.weight.unwrap()
+        );
+    }
+
+    #[test]
+    fn card_weight_in_three_cards_sample() {
+        let sample = DivinationCardsSample::create(
+            SampleData::Csv(String::from("name,amount\rRain of Chaos,2\rThe Doctor,1")),
+            None,
+        )
+        .unwrap();
+
+        let doctor = sample.cards.get_card("The Doctor");
+        assert_eq!(
+            //42944.715-ish
+            (RAIN_OF_CHAOS_WEIGHT / 2.0).powf(1.0 / CONDENSE_FACTOR),
+            doctor.weight.unwrap()
+        );
+    }
+
+    #[test]
+    fn huge_sample() {
+        let data = SampleData::Csv(fs::read_to_string("example-2.csv").unwrap());
+        let sample = DivinationCardsSample::create(data, None).unwrap();
+
+        let fox = sample.cards.get_card("The Fox in the Brambles");
+        assert_eq!(557.44556, fox.weight.unwrap());
+    }
+}
+
+#[cfg(test)]
+mod fix_typos {
+    use crate::sample::{DivinationCardsSample, SampleData};
+    use std::fs;
+    #[test]
+    fn fix_typos() {
+        let sample_data = SampleData::Csv(fs::read_to_string("example-3.csv").unwrap());
+        let sample = DivinationCardsSample::create(sample_data, None).unwrap();
+        assert_eq!(sample.fixed_names.len(), 26);
+    }
+}
