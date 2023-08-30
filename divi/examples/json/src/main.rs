093e952cf836b9c2c5d1980ab5ddaf71b90ec583
shonya3 <poeshonya3@gmail.com>
2023-08-30 13:18:53 +0300

    divi: update example

--- divi/examples/json/src/main.rs
+++ divi/examples/json/src/main.rs
@@ -3,7 +3,7 @@ use divi::{
     sample::{CardNameAmount, DivinationCardsSample, SampleData},
 };
 fn main() -> Result<(), divi::error::Error> {
-    let sample_1 = DivinationCardsSample::create(
+    let s1 = DivinationCardsSample::create(
         SampleData::CardNameAmountList(vec![
             CardNameAmount::new(String::from("Rain of Chaos"), 25),
             CardNameAmount::new(String::from("The Doctor"), 1),
@@ -15,13 +15,12 @@ fn main() -> Result<(), divi::error::Error> {
         {"name": "Rain of Chaos", "amount": 30},
         {"name": "Not really a card", "amount": 1}
     ]"#;
-    let vec: Vec<CardNameAmount> = serde_json::from_str(json).unwrap();
-    let sample_2 = DivinationCardsSample::create(SampleData::CardNameAmountList(vec), None)?;
+    let vec: Vec<CardNameAmount> = serde_json::from_str(json)?;
+    let s2 = DivinationCardsSample::create(SampleData::CardNameAmountList(vec), None)?;
 
-    let merged =
-        DivinationCardsSample::merge(Some(Prices::default()), &[sample_1, sample_2.clone()]);
+    let merged = DivinationCardsSample::merge(Some(Prices::default()), &[s1, s2.clone()]);
 
-    assert_eq!(sample_2.not_cards, vec![String::from("Not really a card")]);
+    assert_eq!(s2.not_cards, vec![String::from("Not really a card")]);
     assert_eq!(merged.cards.get_card("Rain of Chaos").amount, 55);
 
     Ok(())
