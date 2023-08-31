4120f484a1e8491aa9746e977bcc705faefacd15
shonya3 <poeshonya3@gmail.com>
2023-08-31 17:07:06 +0300

    divi: add doc overview

--- divi/examples/prices/src/main.rs
+++ divi/examples/prices/src/main.rs
@@ -7,11 +7,9 @@ use divi::{
 #[tokio::main]
 async fn main() -> Result<(), divi::error::Error> {
     let prices = Prices::fetch(&TradeLeague::Standard).await?;
-    let csv = r"#
-        name,amount
-        The Doctor, 2
-        Rain of Chaos, 30
-    #";
+    let csv = r#"name,amount
+    The Doctor,2
+    Rain of Chaos,30"#;
     let sample = DivinationCardsSample::create(SampleData::Csv(String::from(csv)), Some(prices))?;
     println!(
         "Total price of The Doctor cards: {}",
--- divi/src/lib.rs
+++ divi/src/lib.rs
@@ -1,3 +1,25 @@
+//! A library for parsing sets of divination cards(samples) and calculating the real weight for each card
+//!
+//!
+//!```
+//!use divi::{
+//!    league::TradeLeague,
+//!    prices::Prices,
+//!    sample::{DivinationCardsSample, SampleData},
+//!};
+//!
+//!#[tokio::main]
+//!async fn main() -> Result<(), divi::error::Error> {
+//!    let prices = Prices::fetch(&TradeLeague::Standard).await?;
+//!    let csv = r#"name,amount
+//!    The Doctor,2
+//!    Rain of Chaos,30"#;
+//!    let sample = DivinationCardsSample::create(SampleData::Csv(String::from(csv)), Some(prices))?;
+//!    // output: The Doctor: DivinationCardRecord { name: "The Doctor", amount: 2, price: Some(869.1), sum: Some(1738.2), weight: Some(2090.8254) }
+//!    println!("The Doctor: {:?}", sample.cards.get_card("The Doctor"));
+//!    Ok(())
+//!}
+//! ```
 use consts::{CARDS, LEGACY_CARDS};
 pub mod card_record;
 pub mod cards;
