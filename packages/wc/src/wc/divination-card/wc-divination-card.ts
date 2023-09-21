acef5f0f1f01b940eb619ea88eab101428d82cd5
shonya3 <poeshonya3@gmail.com>
2023-09-21 10:49:41 +0300

    wc-divination-card: add small size

--- packages/wc/src/wc/divination-card/wc-divination-card.ts
+++ packages/wc/src/wc/divination-card/wc-divination-card.ts
@@ -1,4 +1,5 @@
-import { html, css, PropertyValues } from 'lit';
+import { classMap } from 'lit/directives/class-map.js';
+import { html, css, PropertyValues, nothing } from 'lit';
 import { html as staticHtml, unsafeStatic } from 'lit/static-html.js';
 import { BaseElement } from '../base-element';
 import { property, state } from 'lit/decorators.js';
@@ -12,7 +13,7 @@ declare global {
 
 import { cardsDataMap } from './data';
 
-export type CardSize = 'medium' | 'large';
+export type CardSize = 'small' | 'medium' | 'large';
 
 export interface Props {
 	name: string;
@@ -28,7 +29,7 @@ export class DivinationCardElement extends BaseElement {
 	static override styles = [this.baseStyles, styles()];
 
 	@property({ reflect: true }) name: string = '';
-	@property({ reflect: true }) size: CardSize = 'medium';
+	@property({ reflect: true }) size: CardSize = 'small';
 
 	@state() stackSize: number = 0;
 	@state()
@@ -36,6 +37,17 @@ export class DivinationCardElement extends BaseElement {
 	@state()
 	artFilename: string = '';
 	@state() rewardHtml: string = '';
+
+	protected nameMarginTop(size: CardSize) {
+		switch (size) {
+			case 'small':
+				return '0rem';
+			case 'medium':
+				return '0rem';
+			case 'large':
+				return '0.4rem';
+		}
+	}
 
 	protected willUpdate(changedProperties: PropertyValues<this>): void {
 		if (changedProperties.has('name')) {
@@ -56,24 +68,35 @@ export class DivinationCardElement extends BaseElement {
 		});
 
 		const nameTopPadding = styleMap({
-			'padding-top': `${this.size === 'medium' ? '0rem' : '0.4rem'}`,
+			'margin-top': this.nameMarginTop(this.size),
 		});
 
 		return html`<div id="element" style=${sizeMap}>
 			<div id="skeleton" style=${sizeMap}></div>
-			<header id="name" style=${nameTopPadding}>${this.name}</header>
+			<header class="${classMap({ size22: this.size === 'small' })}" id="name" style=${nameTopPadding}>
+				${this.name}
+			</header>
 			<div id="imageWrapper">
 				<img loading="lazy" id="image" width="100%" src="/images/cards/${this.artFilename}.png" alt="" />
 			</div>
-			<div id="stackSize">${this.stackSize}</div>
-			<div id="bottom-half">
+			<div class=${classMap({ stackSize: true, 'stackSize--small': this.size === 'small' })}>
+				${this.stackSize}
+			</div>
+			<div class="${classMap({ size25: this.size === 'small' })}" id="bottom-half">
 				${staticHtml`${unsafeStatic(this.rewardHtml)}`}
-				<div id="divider"></div>
-				<footer>
-					<div style="font-style: italic" id="flavourText">${this.flavourText}</div>
-				</footer>
+				${this.size !== 'small' ? html`${this.divider()}${this.footer()}` : nothing}
 			</div>
 		</div>`;
+	}
+
+	protected divider() {
+		return html`<div id="divider"></div>`;
+	}
+
+	protected footer() {
+		return html`<footer>
+			<p style="font-style: italic" class="flavourText">${this.flavourText}</p>
+		</footer>`;
 	}
 }
 
@@ -82,11 +105,14 @@ function styles() {
 		:host {
 			display: block;
 
-			--card-width-large: 326px;
-			--card-height-large: 501px;
+			--card-width-small: 134px;
+			--card-height-small: 200px;
 
 			--card-width-medium: 268px;
 			--card-height-medium: 401px;
+
+			--card-width-large: 326px;
+			--card-height-large: 501px;
 
 			--item-normal: 0, 0%, 78%;
 			--item-magic: 240, 100%, 77%;
@@ -113,6 +139,122 @@ function styles() {
 			--item-chaos: 322, 73%, 47%;
 			--item-augmented: rgb(138, 138, 255);
 			--coolgrey-1000: 206, 24%, 7%;
+		}
+
+		* {
+			margin: 0;
+			padding: 0;
+		}
+
+		#element {
+			font-family: 'fontin', Verdana, Arial;
+
+			width: var(--card-width, var(--card-width-medium));
+			height: var(--card-height, var(--card-height-medium));
+
+			text-align: center;
+			overflow: hidden;
+
+			display: flex;
+			flex-direction: column;
+
+			position: relative;
+		}
+
+		#skeleton {
+			background: rgba(0, 0, 0, 0) url(/images/cards/divination-card.png) no-repeat center;
+			background-size: 105%;
+			z-index: 3;
+			position: absolute;
+			height: 401px;
+			width: 100%;
+
+			width: var(--card-width, var(--card-width-medium));
+			height: var(--card-height, var(--card-height-medium));
+		}
+
+		#name {
+			color: hsl(var(--item-divination));
+			z-index: 4;
+			color: #111;
+		}
+
+		#imageWrapper {
+			overflow-y: hidden;
+			height: 100px;
+
+			position: absolute;
+			left: 4%;
+			top: 8%;
+			height: 42%;
+			width: 90%;
+			overflow: hidden;
+		}
+
+		#image {
+			display: block;
+			object-fit: contain;
+			min-width: 100%;
+			height: 100%;
+			overflow-y: hidden;
+		}
+
+		.stackSize {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			position: absolute;
+			color: #c8c8c8;
+			left: 8%;
+			top: 46.8%;
+			z-index: 4;
+			width: 16%;
+			font-size: 1rem;
+			height: 26px;
+		}
+
+		.stackSize--small {
+			top: 42.8%;
+			font-size: 0.6rem;
+		}
+
+		#bottom-half {
+			position: absolute;
+			top: 52%;
+			height: 44%;
+			width: 90%;
+			z-index: 4;
+			margin: 0 6% 6%;
+
+			margin-top: 0.4rem;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-evenly;
+
+			flex: 1;
+		}
+
+		.reward {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+		}
+
+		.flavourText {
+			color: rgba(167, 90, 27, 1);
+			text-wrap: balance;
+			font-style: italic;
+			line-height: 1.2rem;
+		}
+
+		#divider {
+			height: 1px;
+			width: 50%;
+			transform: translateX(50%);
+
+			background-image: linear-gradient(to right, transparent, rgba(255, 255, 255, 0.5), transparent);
 		}
 
 		.currencyItem {
@@ -161,123 +303,65 @@ function styles() {
 			color: hsla(var(--item-gem));
 		}
 
-		* {
-			margin: 0;
-			padding: 0;
+		.size22 {
+			font-size: 11px;
+			line-height: 0.8rem;
 		}
 
-		#element {
-			font-family: 'fontin', Verdana, Arial;
-			width: 10vw;
-			min-height: 25vh;
-			height: 401px;
-
-			width: var(--card-width, var(--card-width-medium));
-			height: var(--card-height, var(--card-height-medium));
-
-			padding: 0.15rem;
-
-			text-align: center;
-			overflow: hidden;
-
-			display: flex;
-			flex-direction: column;
-
-			position: relative;
+		.size25 {
+			font-size: 12.5px;
+			line-height: 0.9rem;
 		}
 
-		#skeleton {
-			background: rgba(0, 0, 0, 0) url(/images/cards/divination-card.png) no-repeat center;
-			background-size: 105%;
-			z-index: 3;
-			position: absolute;
-			height: 401px;
-			width: 100%;
-
-			width: var(--card-width, var(--card-width-medium));
-			height: var(--card-height, var(--card-height-medium));
+		.size26 {
+			font-size: 13px;
 		}
 
-		#name {
-			color: hsl(var(--item-divination));
-			padding-top: 0.4rem;
-			padding-bottom: 0.6rem;
-			z-index: 4;
-			color: #111;
+		.size27 {
+			font-size: 13.5px;
+		}
+		.size28 {
+			font-size: 14px;
 		}
 
-		#imageWrapper {
-			overflow-y: hidden;
-			height: 100px;
-
-			position: absolute;
-			left: 4%;
-			top: 8%;
-			height: 42%;
-			width: 90%;
-			overflow: hidden;
+		.size29 {
+			font-size: 14.5px;
 		}
 
-		#image {
-			display: block;
-			object-fit: contain;
-			min-width: 100%;
-			height: 100%;
-			overflow-y: hidden;
+		.size30 {
+			font-size: 15px;
 		}
 
-		#stackSize {
-			display: flex;
-			align-items: center;
-			justify-content: center;
-
-			position: absolute;
-			color: #c8c8c8;
-			left: 8%;
-			top: 46.8%;
-			z-index: 4;
-			width: 16%;
-			font-size: 1rem;
-			height: 26px;
+		.size31 {
+			font-size: 15.5px;
 		}
 
-		#bottom-half {
-			position: absolute;
-			top: 52%;
-			height: 44%;
-			width: 90%;
-			z-index: 4;
-			margin: 0 6% 6%;
-
-			margin-top: 0.4rem;
-			display: flex;
-			flex-direction: column;
-			justify-content: space-evenly;
-
-			flex: 1;
-
-			line-height: 1.3rem;
+		p {
+			line-height: inherit;
 		}
 
-		.reward {
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			justify-content: center;
+		p:has(.size25) {
+			line-height: 0.9rem;
 		}
 
-		#flavourText {
-			color: rgba(167, 90, 27, 1);
-			text-wrap: balance;
-			font-style: italic;
+		p:has(.size26) {
+			line-height: 0.95rem;
 		}
 
-		#divider {
-			height: 1px;
-			width: 50%;
-			transform: translateX(50%);
+		p:has(.size27) {
+			line-height: 1rem;
+		}
 
-			background-image: linear-gradient(to right, transparent, rgba(255, 255, 255, 0.5), transparent);
+		p:has(.size28) {
+			line-height: 1.05rem;
+		}
+
+		p:has(.size29) {
+			line-height: 1.1rem;
+		}
+
+		p:has(.size30) {
+			line-height: 1.15rem;
 		}
 
 		footer {
