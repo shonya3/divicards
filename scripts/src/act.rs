b40cc81e58015c3e1d7f68f524604c4f8c200faf
shonya3 <poeshonya3@gmail.com>
2023-10-12 15:25:54 +0300

    acts loader

--- scripts/src/act.rs
+++ scripts/src/act.rs
@@ -4,10 +4,11 @@ use std::{
     path::Path,
 };
 
+use async_trait::async_trait;
 use playwright::{api::DocumentLoadState, Playwright};
 use serde::{Deserialize, Serialize};
 
-use crate::error::Error;
+use crate::{error::Error, loader::DataLoader};
 
 pub const TOWN_IMAGE_URL: &'static str =
     "https://cdn.poedb.tw/image/Art/2DArt/UIImages/InGame/WorldPanelTownPinIcon.webp";
@@ -16,7 +17,7 @@ pub const WAYPOINT_IMAGE_URL: &'static str =
 pub const LABYRINTH_TRIAL_IMAGE_URL: &'static str =
     "https://cdn.poedb.tw/image/Art/2DArt/UIImages/InGame/WorldPanelLabyrinthWaypointPinIcon.webp";
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct ActArea {
     pub id: String,
@@ -39,7 +40,29 @@ pub struct Bossfight {
 }
 
 impl ActArea {
-    pub async fn collect_acts_data() -> Vec<ActArea> {
+    fn act_url(act: u8) -> String {
+        format!("https://poedb.tw/us/Act_{act}")
+    }
+}
+
+pub struct ActsLoader(reqwest::Client);
+impl ActsLoader {
+    pub const fn new(client: reqwest::Client) -> Self {
+        Self(client)
+    }
+}
+
+#[async_trait]
+impl DataLoader<Vec<ActArea>> for ActsLoader {
+    fn filename(&self) -> &'static str {
+        "acts.json"
+    }
+
+    fn reload(&self) -> bool {
+        false
+    }
+
+    async fn fetch(&self) -> Result<Vec<ActArea>, Error> {
         let script = format!(
             "(el) => {{{} return extractActAreaPopupData(el)}}",
             &std::fs::read_to_string("extractActAreaPopupData.js").unwrap()
@@ -89,93 +112,9 @@ impl ActArea {
             }
         }
 
-        act_areas
-    }
-
-    pub fn download_images(urls: Vec<String>) -> Result<(), Error> {
-        let act_images_dir = env::current_dir()
-            .unwrap()
-            .join("public")
-            .join("images")
-            .join("acts");
-
-        if !act_images_dir.exists() {
-            std::fs::create_dir_all(&act_images_dir).unwrap();
-        }
-
-        tokio::task::spawn_blocking(move || {
-            for url in urls {
-                let (_, filename) = url.rsplit_once("/").unwrap();
-                let path = act_images_dir.join(filename);
-                let mut file = File::create(path).unwrap();
-                let _ = reqwest::blocking::get(url)
-                    .unwrap()
-                    .copy_to(&mut file)
-                    .unwrap();
-            }
-        });
-
-        Ok(())
-    }
-
-    fn act_url(act: u8) -> String {
-        format!("https://poedb.tw/us/Act_{act}")
-    }
-
-    pub fn image_url(act: u8, id: &str) -> String {
-        let (_, area) = id.split_once("_").unwrap();
-        format!("https://cdn.poedb.tw/image/Art/2DArt/UIImages/InGame/Acts/{act}/{area}.webp")
-    }
-
-    async fn _act_html(act: u8) -> Result<String, Error> {
-        Ok(reqwest::get(ActArea::act_url(act)).await?.text().await?)
-    }
-
-    async fn _act1_html() -> String {
-        let dir = env::current_dir().unwrap().join("files");
-        let html_path = dir.join("act1.html");
-        if !dir.exists() {
-            fs::create_dir(&dir).unwrap();
-        };
-
-        let html = match std::fs::read_to_string(&html_path) {
-            Ok(html) => html,
-            Err(_) => {
-                let html = reqwest::get(ActArea::act_url(1))
-                    .await
-                    .unwrap()
-                    .text()
-                    .await
-                    .unwrap();
-
-                std::fs::write(&dir.join(&html_path), &html).unwrap();
-                html
-            }
-        };
-
-        html
-    }
-
-    fn _write<P: AsRef<Path>>(areas: Vec<Self>, path: P) {
-        fs::write(path, serde_json::to_string_pretty(&areas).unwrap()).unwrap();
-    }
-
-    pub fn _load_areas_from_file<P: AsRef<Path>>(path: P) -> Vec<Self> {
-        let s = std::fs::read_to_string(path).unwrap();
-        serde_json::from_str(&s).unwrap()
+        Ok(act_areas)
     }
 }
-
-// #[tokio::main]
-// pub async fn main() {
-//     let areas = ActArea::_load_areas_from_file("files/areas.json");
-//     dbg!(areas);
-// }
-
-/// let areas = ActArea::_load_areas_from_file("files/areas.json");
-///     for area in ACT_AREA_NAMES {
-///        find_by_name(area, &areas)
-///     }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct AreaNameAct {
@@ -289,3 +228,29 @@ pub fn parse_area_name(s: &str) -> Vec<AreaNameAct> {
 }
 
 // fn area_id_from_str(s: &str) {}
+
+pub fn download_images(urls: Vec<String>) -> Result<(), Error> {
+    let act_images_dir = env::current_dir()
+        .unwrap()
+        .join("public")
+        .join("images")
+        .join("acts");
+
+    if !act_images_dir.exists() {
+        std::fs::create_dir_all(&act_images_dir).unwrap();
+    }
+
+    tokio::task::spawn_blocking(move || {
+        for url in urls {
+            let (_, filename) = url.rsplit_once("/").unwrap();
+            let path = act_images_dir.join(filename);
+            let mut file = File::create(path).unwrap();
+            let _ = reqwest::blocking::get(url)
+                .unwrap()
+                .copy_to(&mut file)
+                .unwrap();
+        }
+    });
+
+    Ok(())
+}
