5d7b97769a88eb99167657981222c8b507c8724d
shonya3 <poeshonya3@gmail.com>
2023-10-19 17:30:39 +0300

    move loading logic down

--- scripts/src/act.rs
+++ scripts/src/act.rs
@@ -41,6 +41,168 @@ impl ActArea {
     }
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct ActAreaDivcordNotation(pub String);
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum ActAreaName {
+    #[serde(untagged)]
+    Name(String),
+    #[serde(untagged)]
+    NameWithAct((String, u8)),
+}
+
+pub fn parse_act_areas(drops_from: &DropsFrom, acts: &[ActArea], min_level: u8) -> Vec<String> {
+    if !drops_from.styles.italic {
+        panic!("Act areas should be italic");
+    }
+
+    let s = &drops_from.name;
+    let names = match is_act_notation(s) {
+        true if s == "The Belly of the Beast (A4/A9)" => vec![
+            ActAreaName::NameWithAct(("The Belly of the Beast Level 1".to_string(), 4)),
+            ActAreaName::NameWithAct(("The Belly of the Beast Level 1".to_string(), 4)),
+            ActAreaName::NameWithAct(("The Belly of the Beast".to_string(), 9)),
+        ],
+        true => parse_act_notation(s),
+        false => vec![ActAreaName::Name(s.to_owned())],
+    };
+
+    names
+        .iter()
+        .flat_map(|name| find_ids(&name, acts, min_level))
+        .collect()
+}
+
+pub fn parse_act_notation(s: &str) -> Vec<ActAreaName> {
+    if !s.contains("(") && !s.contains("/") {
+        panic!("Expected act notation, got {s}");
+    };
+
+    let mut split = s.split("(");
+
+    let name = split.next().expect("No name, {s}");
+    let mut names: Vec<String> = Vec::new();
+
+    if name.contains("1/2") {
+        let name = name.replace("1/2", "");
+        let name = name.trim();
+        for n in [1, 2] {
+            let name = format!("{name} {n}");
+            names.push(name);
+        }
+    } else {
+        names.push(name.to_string());
+    }
+
+    let names = match name.contains("1/2") {
+        true => {
+            let name = name.replace("1/2", "");
+            let name = name.trim();
+            [1, 2].iter().map(|n| format!("{name} {n}")).collect()
+        }
+        false => vec![name.trim().to_string()],
+    };
+
+    if let Some(acts) = split.next() {
+        if acts.contains("/") {
+            let (left, right) = acts.split_once("/").unwrap();
+
+            let left = left
+                .chars()
+                .into_iter()
+                .filter(|c| c.is_digit(10))
+                .collect::<String>()
+                .parse::<u8>()
+                .unwrap();
+
+            let right = right
+                .chars()
+                .into_iter()
+                .filter(|c| c.is_digit(10))
+                .collect::<String>()
+                .parse::<u8>()
+                .unwrap();
+
+            names
+                .into_iter()
+                .flat_map(|name| {
+                    [
+                        ActAreaName::NameWithAct((name.clone(), left)),
+                        ActAreaName::NameWithAct((name, right)),
+                    ]
+                })
+                .collect()
+        } else {
+            let left = acts
+                .chars()
+                .into_iter()
+                .filter(|c| c.is_digit(10))
+                .collect::<String>()
+                .parse::<u8>()
+                .unwrap();
+
+            names
+                .into_iter()
+                .map(|name| ActAreaName::NameWithAct((name, left)))
+                .collect()
+        }
+    } else {
+        names
+            .into_iter()
+            .map(|name| ActAreaName::Name(name))
+            .collect()
+    }
+}
+
+pub fn find_ids(name: &ActAreaName, acts: &[ActArea], min_level: u8) -> Vec<String> {
+    match name {
+        ActAreaName::Name(name) => acts
+            .iter()
+            .filter(|a| &a.name == name && a.is_town == false && a.area_level >= min_level)
+            .map(|a| a.id.to_owned())
+            .collect(),
+        ActAreaName::NameWithAct((name, act)) => {
+            let mut v = vec![];
+            if let Some(a) = acts
+                .iter()
+                .find(|a| &a.name == name && &a.act == act && a.area_level >= min_level)
+            {
+                v.push(a.id.to_owned())
+            };
+
+            v
+        }
+    }
+}
+
+pub fn download_images(urls: Vec<String>) -> Result<(), Error> {
+    let act_images_dir = env::current_dir()
+        .unwrap()
+        .join("public")
+        .join("images")
+        .join("acts");
+
+    if !act_images_dir.exists() {
+        std::fs::create_dir_all(&act_images_dir).unwrap();
+    }
+
+    tokio::task::spawn_blocking(move || {
+        for url in urls {
+            let (_, filename) = url.rsplit_once("/").unwrap();
+            let path = act_images_dir.join(filename);
+            let mut file = File::create(path).unwrap();
+            let _ = reqwest::blocking::get(url)
+                .unwrap()
+                .copy_to(&mut file)
+                .unwrap();
+        }
+    });
+
+    Ok(())
+}
+
 pub struct ActsLoader(reqwest::Client);
 impl ActsLoader {
     pub const fn new(client: reqwest::Client) -> Self {
@@ -110,167 +272,4 @@ impl DataLoader<Vec<ActArea>> for ActsLoader {
 
         Ok(act_areas)
     }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, Default)]
-#[serde(rename_all = "camelCase")]
-pub struct ActAreaDivcordNotation(pub String);
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub enum ActAreaName {
-    #[serde(untagged)]
-    Name(String),
-    #[serde(untagged)]
-    NameWithAct((String, u8)),
-}
-
-pub fn parse_act_notation(s: &str) -> Vec<ActAreaName> {
-    if !s.contains("(") && !s.contains("/") {
-        panic!("Expected act notation, got {s}");
-    };
-
-    let mut split = s.split("(");
-
-    let name = split.next().expect("No name, {s}");
-    let mut names: Vec<String> = Vec::new();
-
-    if name.contains("1/2") {
-        let name = name.replace("1/2", "");
-        let name = name.trim();
-        for n in [1, 2] {
-            let name = format!("{name} {n}");
-            // println!("PUSHING {name}");
-            names.push(name);
-        }
-    } else {
-        names.push(name.to_string());
-    }
-
-    let names = match name.contains("1/2") {
-        true => {
-            let name = name.replace("1/2", "");
-            let name = name.trim();
-            [1, 2].iter().map(|n| format!("{name} {n}")).collect()
-        }
-        false => vec![name.trim().to_string()],
-    };
-
-    if let Some(acts) = split.next() {
-        if acts.contains("/") {
-            let (left, right) = acts.split_once("/").unwrap();
-
-            let left = left
-                .chars()
-                .into_iter()
-                .filter(|c| c.is_digit(10))
-                .collect::<String>()
-                .parse::<u8>()
-                .unwrap();
-
-            let right = right
-                .chars()
-                .into_iter()
-                .filter(|c| c.is_digit(10))
-                .collect::<String>()
-                .parse::<u8>()
-                .unwrap();
-
-            names
-                .into_iter()
-                .flat_map(|name| {
-                    [
-                        ActAreaName::NameWithAct((name.clone(), left)),
-                        ActAreaName::NameWithAct((name, right)),
-                    ]
-                })
-                .collect()
-        } else {
-            let left = acts
-                .chars()
-                .into_iter()
-                .filter(|c| c.is_digit(10))
-                .collect::<String>()
-                .parse::<u8>()
-                .unwrap();
-
-            names
-                .into_iter()
-                .map(|name| ActAreaName::NameWithAct((name, left)))
-                .collect()
-        }
-    } else {
-        names
-            .into_iter()
-            .map(|name| ActAreaName::Name(name))
-            .collect()
-    }
-}
-
-pub fn parse_act_areas(drops_from: &DropsFrom, acts: &[ActArea], min_level: u8) -> Vec<String> {
-    if !drops_from.styles.italic {
-        panic!("Act areas should be italic");
-    }
-
-    let s = &drops_from.name;
-    let names = match is_act_notation(s) {
-        true if s == "The Belly of the Beast (A4/A9)" => vec![
-            ActAreaName::NameWithAct(("The Belly of the Beast Level 1".to_string(), 4)),
-            ActAreaName::NameWithAct(("The Belly of the Beast Level 1".to_string(), 4)),
-            ActAreaName::NameWithAct(("The Belly of the Beast".to_string(), 9)),
-        ],
-        true => parse_act_notation(s),
-        false => vec![ActAreaName::Name(s.to_owned())],
-    };
-
-    names
-        .iter()
-        .flat_map(|name| find_ids(&name, acts, min_level))
-        .collect()
-}
-
-pub fn find_ids(name: &ActAreaName, acts: &[ActArea], min_level: u8) -> Vec<String> {
-    match name {
-        ActAreaName::Name(name) => acts
-            .iter()
-            .filter(|a| &a.name == name && a.is_town == false && a.area_level >= min_level)
-            .map(|a| a.id.to_owned())
-            .collect(),
-        ActAreaName::NameWithAct((name, act)) => {
-            let mut v = vec![];
-            if let Some(a) = acts
-                .iter()
-                .find(|a| &a.name == name && &a.act == act && a.area_level >= min_level)
-            {
-                v.push(a.id.to_owned())
-            };
-
-            v
-        }
-    }
-}
-
-pub fn download_images(urls: Vec<String>) -> Result<(), Error> {
-    let act_images_dir = env::current_dir()
-        .unwrap()
-        .join("public")
-        .join("images")
-        .join("acts");
-
-    if !act_images_dir.exists() {
-        std::fs::create_dir_all(&act_images_dir).unwrap();
-    }
-
-    tokio::task::spawn_blocking(move || {
-        for url in urls {
-            let (_, filename) = url.rsplit_once("/").unwrap();
-            let path = act_images_dir.join(filename);
-            let mut file = File::create(path).unwrap();
-            let _ = reqwest::blocking::get(url)
-                .unwrap()
-                .copy_to(&mut file)
-                .unwrap();
-        }
-    });
-
-    Ok(())
 }
