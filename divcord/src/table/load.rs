78352adf8ab529298bf6518d062b8a00958a6566
shonya3 <poeshonya3@gmail.com>
2023-11-03 19:16:41 +0300

    talbe loader: add _fetch

--- divcord/src/table/load.rs
+++ divcord/src/table/load.rs
@@ -14,16 +14,13 @@ impl DivcordTableLoader {
         Self(reqwest::Client::new())
     }
 
-    pub async fn fetch_rich_sources_column(
-        &self,
-        client: &reqwest::Client,
-    ) -> Result<RichSourcesColumn, Error> {
+    pub async fn fetch_rich_sources_column(&self) -> Result<RichSourcesColumn, Error> {
         dotenv::dotenv().ok();
         let key = std::env::var("GOOGLE_API_KEY").expect("No google api key");
         let spreadsheet_id = "1Pf2KNuGguZLyf6eu_R0E503U0QNyfMZqaRETsN5g6kU";
         let sheet = "Cards_and_Hypotheses";
         let url = format!("https://sheets.googleapis.com/v4/spreadsheets/{spreadsheet_id}?&ranges={sheet}!F3:F&includeGridData=true&key={key}");
-        Ok(client.get(url).send().await?.json().await?)
+        Ok(self.0.get(url).send().await?.json().await?)
     }
 
     pub async fn fetch_table_sheet(&self, client: &reqwest::Client) -> Result<ValueRange, Error> {
@@ -39,7 +36,21 @@ impl DivcordTableLoader {
         let value_range: ValueRange = client.get(url).send().await?.json().await?;
         Ok(value_range)
     }
+
+    pub async fn _fetch(&self) -> Result<DivcordTable, Error> {
+        let sheet = self.fetch_table_sheet(&self.0).await?;
+        let rich_sources_column = RichSourcesColumn::new(
+            self.fetch_rich_sources_column().await?.sheets,
+            sheet.values.len(),
+        );
+
+        Ok(DivcordTable {
+            sheet,
+            rich_sources_column,
+        })
+    }
 }
+
 #[async_trait]
 impl DataLoader<DivcordTable, Error> for DivcordTableLoader {
     fn filename(&self) -> &'static str {
@@ -47,15 +58,6 @@ impl DataLoader<DivcordTable, Error> for DivcordTableLoader {
     }
 
     async fn fetch(&self) -> Result<DivcordTable, Error> {
-        let sheet = self.fetch_table_sheet(&self.0).await?;
-        let len = sheet.values.len();
-
-        Ok(DivcordTable {
-            sheet: self.fetch_table_sheet(&self.0).await?,
-            rich_sources_column: RichSourcesColumn::new(
-                self.fetch_rich_sources_column(&self.0).await?.sheets,
-                len,
-            ),
-        })
+        self._fetch().await
     }
 }
