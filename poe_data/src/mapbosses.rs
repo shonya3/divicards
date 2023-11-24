5b224c283d9d107fc2cf8456214e8e5b338fec50
shonya3 <poeshonya3@gmail.com>
2023-11-24 06:52:33 +0300

    embed js scripts with include_str!

--- poe_data/src/mapbosses.rs
+++ poe_data/src/mapbosses.rs
@@ -11,68 +11,10 @@ pub async fn fetch() -> Result<Vec<MapBoss>, crate::error::Error> {
     use crate::consts::MAPBOSSES_POEWIKI_URL;
     use playwright::Playwright;
 
-    let js = r#"
-    /**
-     * @typedef {Object} MapBoss
-     * @property {string} name
-     * @property {string[]} maps
-     */
-
-    /**
-     * Fetch bosses from https://www.poewiki.net/wiki/Map_bosses
-     * @returns {MapBoss[]}
-     */
-    const fetchMapBosses = () => {
-        const tableRows = document.querySelector('table')?.querySelector('tbody')?.querySelectorAll('tr');
-        if (tableRows == null) {
-            throw new Error('No table');
-        }
-
-        /** @type Map<string, string[]> */
-        const dataMap = new Map();
-
-        for (const row of tableRows) {
-            const [nameCol, mapCol] = row.querySelectorAll('td');
-            const boss = nameCol.innerText.trim();
-            const map = mapCol.innerText.trim();
-
-            const maps = dataMap.get(boss) ?? [];
-            maps.push(map);
-            dataMap.set(boss, maps);
-        }
-
-        /** @type{MapBoss[]} */
-        const mapBosses = [];
-        for (const [n, maps] of dataMap) {
-            let name = n;
-
-            switch (name) {
-                case 'Drought Maddened Rhoa':
-                    name = 'Drought-Maddened Rhoa';
-                    break;
-                case 'Hephaestus, the Hammer':
-                    name = 'Hephaeus, The Hammer';
-                    break;
-                case 'Kitava, the Destroyer':
-                    name = 'Kitava, The Destroyer';
-                    console.log(n);
-                    break;
-            }
-
-            mapBosses.push({ name, maps });
-        }
-
-        const cleansingLight = { name: 'The Cleansing Light', maps: ['Basilica Map'] };
-        const opid = { name: "Opid, Helial's Herald", maps: ['The Twilight Temple'] };
-        const vindicatedQueen = { name: 'The Vindicated Queen', maps: ['Caldera Map'] };
-
-        mapBosses.push(cleansingLight, opid, vindicatedQueen);
-
-        return mapBosses;
-    };
-    "#;
-
-    let script = format!("() => {{{js} return fetchMapBosses()}}");
+    let script = format!(
+        "() => {{{} return fetchMapBosses()}}",
+        include_str!("../fetchMapBosses.js")
+    );
     let playwright = Playwright::initialize().await.unwrap();
     playwright.install_chromium().unwrap();
     let chrome = playwright.chromium();
