cdea58d08647dfb023836bc8a7efe3a950f971f3
shonya3 <poeshonya3@gmail.com>
2023-12-14 08:02:01 +0300

    update divcord cards api

--- divcord-wasm/src/lib.rs
+++ divcord-wasm/src/lib.rs
@@ -33,3 +33,42 @@ pub fn parsed_records(
 
     Ok(serde_wasm_bindgen::to_value(&records).unwrap())
 }
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn find_cards_by_source_types(types: JsValue, records: JsValue) -> JsValue {
+    set_panic_hook();
+    let poe_data_json = include_str!("../poeData.json");
+    let poe_data: PoeData = serde_json::from_str(poe_data_json).unwrap();
+
+    let types: Vec<String> = serde_wasm_bindgen::from_value(types).unwrap();
+    let records: Vec<SourcefulDivcordTableRecord> =
+        serde_wasm_bindgen::from_value(records).unwrap();
+
+    let cards = divcord::cards::find_cards_by_source_types(&types, &records, &poe_data);
+
+    serde_wasm_bindgen::to_value(&cards).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn find_cards_by_source_types_argument(
+    types: JsValue,
+    records: JsValue,
+    poe_data: JsValue,
+) -> JsValue {
+    set_panic_hook();
+    let poe_data: PoeData = serde_wasm_bindgen::from_value(poe_data).unwrap();
+
+    let types: Vec<String> = serde_wasm_bindgen::from_value(types).unwrap();
+    let records: Vec<SourcefulDivcordTableRecord> =
+        serde_wasm_bindgen::from_value(records).unwrap();
+
+    let cards = divcord::cards::find_cards_by_source_types(&types, &records, &poe_data);
+
+    serde_wasm_bindgen::to_value(&cards).unwrap()
+}
--- divcord/src/cards.rs
+++ divcord/src/cards.rs
@@ -62,7 +62,7 @@ pub fn cards_by_actboss(boss: &str, records: &[SourcefulDivcordTableRecord]) -> 
 #[serde(untagged)]
 pub enum CFromSource {
     Card { card: String },
-    WithBoss { card: String, boss: String },
+    WithBoss { card: String, boss: Source },
 }
 
 pub fn find_cards_by_source_types(
@@ -96,7 +96,9 @@ pub fn find_cards_by_source_types(
                             for card in cards_by_mapboss(&boss.name, records, poe_data) {
                                 entry.push(CFromSource::WithBoss {
                                     card,
-                                    boss: boss.name.to_owned(),
+                                    boss: Source::MapBoss {
+                                        name: boss.name.clone(),
+                                    },
                                 });
                             }
                         }
@@ -110,7 +112,9 @@ pub fn find_cards_by_source_types(
                             for card in cards_by_actboss(&fight.name, records) {
                                 entry.push(CFromSource::WithBoss {
                                     card,
-                                    boss: fight.name.clone(),
+                                    boss: Source::ActBoss {
+                                        name: fight.name.clone(),
+                                    },
                                 })
                             }
                         }
@@ -129,85 +133,3 @@ pub fn find_cards_by_source_types(
         })
         .collect()
 }
-
-// pub fn find_cards_by_source_types(
-//     source_types: &[String],
-//     records: &[SourcefulDivcordTableRecord],
-//     poe_data: &PoeData,
-// ) -> Vec<SourceAndCards> {
-//     // let mut map: HashMap<String, (&Source, Vec<CFromSource>)> = HashMap::new();
-//     let mut map: HashMap<String, Vec<CFromSource>> = HashMap::new();
-//     let mut sourcemap: HashMap<String, &Source> = HashMap::new();
-//     let mut set: HashSet<&str> = HashSet::new();
-//     // let mut i = 0;
-
-//     // let source_types_as_str: Vec<&str> = source_types.iter().map(|t| t.as_str()).collect();
-
-//     for record in records {
-//         // let filtered_sources = record
-//         //     .sources
-//         //     .iter()
-//         //     .filter(|s| source_types_as_str.contains(&s._type()))
-//         //     .collect::<Vec<_>>();
-
-//         for source in &record.sources {
-//             // sourcemap.insert(source._id(), source);
-//             // let entry = map.entry(source._id()).or_default();
-//             // entry.push(CFromSource::Card {
-//             //     card: record.card.to_owned(),
-//             // });
-
-//             // let entry = map.entry(source._id()).or_insert((source, vec![]));
-//             // entry.1.push(CFromSource::Card {
-//             //     card: record.card.to_owned(),
-//             // });
-
-//             // if let Source::Map { name } = source {
-//             //     if !set.contains(name.as_str()) {
-//             //         set.insert(name);
-//             //         for boss in bosses_in_map(name, poe_data) {
-//             //             for card in cards_by_mapboss(&boss.name, records, poe_data) {
-//             //                 entry.push(CFromSource::WithBoss {
-//             //                     card,
-//             //                     boss: boss.name.to_owned(),
-//             //                 });
-//             //             }
-//             //         }
-//             //     }
-//             // };
-
-//             // if let Source::Act { id } = source {
-//             //     let act_area = poe_data.acts.iter().find(|a| a.id == *id).unwrap();
-//             //     if !set.contains(id.as_str()) {
-//             //         set.insert(id);
-//             //         for fight in &act_area.bossfights {
-//             //             for card in cards_by_actboss(&fight.name, records) {
-//             //                 entry.push(CFromSource::WithBoss {
-//             //                     card,
-//             //                     boss: fight.name.clone(),
-//             //                 })
-//             //             }
-//             //         }
-//             //     }
-//             // };
-//         }
-//     }
-
-//     // dbg!(i);
-
-//     map.into_iter()
-//         .map(|(id, cards)| {
-//             let s = sourcemap.get(&id).unwrap();
-//             let s = <Source>::clone(s);
-//             SourceAndCards { source: s, cards }
-//         })
-//         .collect()
-
-//     // map.into_iter()
-//     //     .map(|(_, (source, cards))| {
-//     //         let s = sourcemap.get(&source._id()).unwrap();
-//     //         let s = <Source>::clone(s);
-//     //         SourceAndCards { source: s, cards }
-//     //     })
-//     //     .collect()
-// }
