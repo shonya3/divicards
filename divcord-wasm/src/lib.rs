637ae48189511343f4e9d7d61441a48c5e65ea94
shonya3 <poeshonya3@gmail.com>
2023-12-16 07:03:27 +0300

    divcord-wasm: deserialize divcord_table with serde_json

--- divcord-wasm/src/lib.rs
+++ divcord-wasm/src/lib.rs
@@ -7,13 +7,13 @@ use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
 #[wasm_bindgen]
 pub fn parsed_records(
-    divcord_table: JsValue,
+    divcord_table: String,
     poe_data: JsValue,
     toast: &js_sys::Function,
 ) -> Result<JsValue, JsValue> {
     set_panic_hook();
 
-    let table: DivcordTable = serde_wasm_bindgen::from_value(divcord_table).unwrap();
+    let table: DivcordTable = serde_json::from_str(&divcord_table).unwrap();
     let poe_data: PoeData = serde_wasm_bindgen::from_value(poe_data).unwrap();
 
     let mut records: Vec<SourcefulDivcordTableRecord> = vec![];
@@ -41,26 +41,7 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn find_cards_by_source_types(types: JsValue, records: JsValue) -> JsValue {
-    set_panic_hook();
-    let poe_data_json = include_str!("../poeData.json");
-    let poe_data: PoeData = serde_json::from_str(poe_data_json).unwrap();
-
-    let types: Vec<String> = serde_wasm_bindgen::from_value(types).unwrap();
-    let records: Vec<SourcefulDivcordTableRecord> =
-        serde_wasm_bindgen::from_value(records).unwrap();
-
-    let cards = divcord::cards::find_cards_by_source_types(&types, &records, &poe_data);
-
-    serde_wasm_bindgen::to_value(&cards).unwrap()
-}
-
-#[wasm_bindgen]
-pub fn find_cards_by_source_types_argument(
-    types: JsValue,
-    records: JsValue,
-    poe_data: JsValue,
-) -> JsValue {
+pub fn find_cards_by_source_types(types: JsValue, records: JsValue, poe_data: JsValue) -> JsValue {
     set_panic_hook();
     let poe_data: PoeData = serde_wasm_bindgen::from_value(poe_data).unwrap();
 
