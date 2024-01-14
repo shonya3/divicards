e5d62986d5c0535a8d60550e94da96353db09a49
shonya3 <poeshonya3@gmail.com>
2024-01-14 20:03:18 +0300

    ok or else

--- packages/app/src-tauri/lib/src/paths.rs
+++ packages/app/src-tauri/lib/src/paths.rs
@@ -2,9 +2,7 @@ use crate::error::Error;
 use std::path::PathBuf;
 
 pub fn appdata_dir() -> Result<PathBuf, Error> {
-    let Some(path) = tauri::api::path::config_dir() else {
-        return Err(Error::ConfigDirNotExists);
-    };
+    let path = tauri::api::path::config_dir().ok_or_else(|| Error::ConfigDirNotExists)?;
 
     if !path.exists() {
         std::fs::create_dir(&path)?;
