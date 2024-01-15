ead9ef3835392c22d8fa54d53b3783ddbe3b7ae2
shonya3 <poeshonya3@gmail.com>
2024-01-15 18:43:05 +0300

    remove old vue directives from template

--- packages/wc/src/wc/file-card/fixed-names/fixed-names.ts
+++ packages/wc/src/wc/file-card/fixed-names/fixed-names.ts
@@ -16,6 +16,45 @@ export class FixedNamesElement extends BaseElement {
 		return [BasePopupElement, FixedIconElement];
 	}
 	static override tag = 'wc-fixed-names';
+
+	@property({ reflect: true, type: Number }) width = 24;
+	@property({ reflect: true, type: Number }) height = 24;
+	@property({ type: Array }) fixedNames: FixedName[] = [];
+
+	@query('wc-base-popup') popup!: BasePopupElement;
+
+	#onIconClicked() {
+		this.popup.open();
+	}
+
+	protected override render() {
+		return html`<wc-fixed-icon
+				@click=${this.#onIconClicked}
+				width=${this.width}
+				height=${this.height}
+			></wc-fixed-icon>
+			<wc-base-popup>
+				<div class="fixed-names">
+					<h2>Automatically fixed typos</h2>
+					<ul class="fixed-names">
+						${this.fixedNames.map(({ old, fixed }) => {
+							return html`<li class="list-item">
+								<span class="input-name">${old}</span>
+								${this.arrowIcon()}
+								<span class="fixed-name">${fixed}</span>
+							</li>`;
+						})}
+					</ul>
+				</div>
+			</wc-base-popup> `;
+	}
+
+	protected arrowIcon() {
+		return html`<svg width="16" height="16" viewBox="0 0 24 24">
+			<path fill="currentColor" d="m14 18l-1.4-1.45L16.15 13H4v-2h12.15L12.6 7.45L14 6l6 6Z"></path>
+		</svg>`;
+	}
+
 	static override styles = [
 		this.baseStyles,
 		css`
@@ -36,42 +75,4 @@ export class FixedNamesElement extends BaseElement {
 			}
 		`,
 	];
-
-	@property({ reflect: true, type: Number }) width = 24;
-	@property({ reflect: true, type: Number }) height = 24;
-	@property({ type: Array }) fixedNames: FixedName[] = [];
-
-	@query('wc-base-popup') popup!: BasePopupElement;
-
-	#onIconClicked() {
-		this.popup.open();
-	}
-
-	protected override render() {
-		return html`<wc-fixed-icon
-				@click=${this.#onIconClicked}
-				width=${this.width}
-				height=${this.height}
-			></wc-fixed-icon>
-			<wc-base-popup ref="fixedNamesPopup">
-				<div class="fixed-names">
-					<h2>Automatically fixed typos</h2>
-					<ul class="fixed-names">
-						${this.fixedNames.map(({ old, fixed }) => {
-							return html`<li class="list-item" v-for="{ old, fixed } in fixedNames">
-								<span class="input-name">${old}</span>
-								${this.arrowIcon()}
-								<span class="fixed-name">${fixed}</span>
-							</li>`;
-						})}
-					</ul>
-				</div>
-			</wc-base-popup> `;
-	}
-
-	protected arrowIcon() {
-		return html`<svg width="16" height="16" viewBox="0 0 24 24">
-			<path fill="currentColor" d="m14 18l-1.4-1.45L16.15 13H4v-2h12.15L12.6 7.45L14 6l6 6Z"></path>
-		</svg>`;
-	}
 }
