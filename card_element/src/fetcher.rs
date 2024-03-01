89d7573a2c026f8a58a5ac19798d9bdc10bea5e6
shonya3 <poeshonya3@gmail.com>
2024-03-01 09:47:11 +0300

    move fetcher to own mod

---
+++ card_element/src/fetcher.rs
@@ -0,0 +1,50 @@
+use crate::{reward::reward_to_html, DivinationCardElementData, Error};
+use divi::{league::TradeLeague, prices::NinjaCardData};
+use fetcher::experimental::{Config, DataFetcher, Stale, WithConfig};
+
+pub struct Fetcher(Config);
+
+impl Default for Fetcher {
+    fn default() -> Self {
+        Self(Config {
+            save: true,
+            filename: "cardElementData.json",
+            stale: Stale::ReloadEveryTime,
+        })
+    }
+}
+
+impl WithConfig for Fetcher {
+    fn config(&self) -> &Config {
+        &self.0
+    }
+}
+
+impl DataFetcher<Vec<DivinationCardElementData>, Error> for Fetcher {
+    async fn fetch(&self) -> Result<Vec<DivinationCardElementData>, Error> {
+        let vec: Vec<NinjaCardData> = NinjaCardData::fetch(&TradeLeague::default()).await?;
+        let v: Vec<DivinationCardElementData> = vec
+            .into_iter()
+            .map(|data| {
+                let mut fl = data.flavour_text;
+                if fl.starts_with("<size:") {
+                    fl = fl[10..fl.len() - 1].to_string();
+                }
+
+                if fl.starts_with("<smaller>{") {
+                    fl = fl[10..fl.len() - 1].to_string();
+                }
+
+                DivinationCardElementData {
+                    name: data.name,
+                    art_filename: data.art_filename,
+                    flavour_text: fl,
+                    stack_size: data.stack_size,
+                    reward_html: reward_to_html(&data.explicit_modifiers[0].text),
+                }
+            })
+            .collect();
+
+        Ok(v)
+    }
+}
--- card_element/src/lib.rs
+++ card_element/src/lib.rs
@@ -1,13 +1,12 @@
 pub mod error;
+pub mod fetcher;
 pub mod images;
 pub mod reward;
 
-use divi::{league::TradeLeague, prices::NinjaCardData};
+use ::fetcher::experimental::{DataFetcher, WithConfig};
 pub use error::Error;
-use fetcher::experimental::{Config, DataFetcher, Stale, WithConfig};
+use fetcher::Fetcher;
 use serde::{Deserialize, Serialize};
-
-use self::reward::reward_to_html;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -21,57 +20,10 @@ pub struct DivinationCardElementData {
 
 impl DivinationCardElementData {
     pub fn filename() -> &'static str {
-        CardElementDataFetcher::default().config().filename
+        Fetcher::default().config().filename
     }
 
     pub async fn load() -> Result<Vec<DivinationCardElementData>, Error> {
-        CardElementDataFetcher::default().load().await
-    }
-}
-
-pub struct CardElementDataFetcher(Config);
-
-impl Default for CardElementDataFetcher {
-    fn default() -> Self {
-        Self(Config {
-            save: true,
-            filename: "cardElementData.json",
-            stale: Stale::ReloadEveryTime,
-        })
-    }
-}
-
-impl WithConfig for CardElementDataFetcher {
-    fn config(&self) -> &Config {
-        &self.0
-    }
-}
-
-impl DataFetcher<Vec<DivinationCardElementData>, Error> for CardElementDataFetcher {
-    async fn fetch(&self) -> Result<Vec<DivinationCardElementData>, Error> {
-        let vec: Vec<NinjaCardData> = NinjaCardData::fetch(&TradeLeague::default()).await?;
-        let v: Vec<DivinationCardElementData> = vec
-            .into_iter()
-            .map(|data| {
-                let mut fl = data.flavour_text;
-                if fl.starts_with("<size:") {
-                    fl = fl[10..fl.len() - 1].to_string();
-                }
-
-                if fl.starts_with("<smaller>{") {
-                    fl = fl[10..fl.len() - 1].to_string();
-                }
-
-                DivinationCardElementData {
-                    name: data.name,
-                    art_filename: data.art_filename,
-                    flavour_text: fl,
-                    stack_size: data.stack_size,
-                    reward_html: reward_to_html(&data.explicit_modifiers[0].text),
-                }
-            })
-            .collect();
-
-        Ok(v)
+        Fetcher::default().load().await
     }
 }
