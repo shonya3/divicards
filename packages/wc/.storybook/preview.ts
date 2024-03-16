d97f8ff3905dc1bd9a63f9b6db41b689c42cf23a
shonya3 <poeshonya3@gmail.com>
2024-03-16 12:45:28 +0300

    Storybook v8

--- packages/wc/.storybook/preview.ts
+++ packages/wc/.storybook/preview.ts
@@ -1,6 +1,12 @@
 import type { Preview } from "@storybook/web-components";
 
+
+import * as styles from './dark.css'
+
+console.log(styles)
+
 const preview: Preview = {
+    h: styles,
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
