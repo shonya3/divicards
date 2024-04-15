9718ed78bd9d671eb6a78af5581b5956fceea480
shonya3 <poeshonya3@gmail.com>
2024-04-15 19:17:57 +0300

    bump storybook

--- packages/wc/.storybook/main.ts
+++ packages/wc/.storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from '@storybook/web-components-vite';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+	addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@chromatic-com/storybook'
+    ],
 	framework: {
 		name: '@storybook/web-components-vite',
 		options: {},
