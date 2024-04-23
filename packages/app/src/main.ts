c5cbb4b45b46254682f2482290aee830f51ae2e9
shonya3 <poeshonya3@gmail.com>
2024-04-23 20:45:46 +0300

    declare global groupBy

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -37,3 +37,17 @@ app.config.errorHandler = handleError;
 addRustListener('toast', e => {
 	toast(e.payload.variant, e.payload.message);
 });
+
+declare global {
+	interface ObjectConstructor {
+		/**
+		 * Groups members of an iterable according to the return value of the passed callback.
+		 * @param items An iterable.
+		 * @param keySelector A callback which will be invoked for each item in items.
+		 */
+		groupBy<K extends PropertyKey, T>(
+			items: Iterable<T>,
+			keySelector: (item: T, index: number) => K
+		): Partial<Record<K, T[]>>;
+	}
+}
