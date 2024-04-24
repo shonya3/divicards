87238f346368a972b9531cce39fc4776972f742d
shonya3 <poeshonya3@gmail.com>
2024-04-24 16:54:02 +0300

    update config

--- packages/wc/custom-elements-manifest.config.js
+++ packages/wc/custom-elements-manifest.config.js
@@ -22,25 +22,6 @@ export default {
 							classDoc.tagName = `wc-${name}`;
 							classDoc.customElement = true;
 						}
-
-						// const classDoc = moduleDoc?.declarations?.find(declaration => declaration.name === className);
-						// console.log(classDoc);
-
-						// const importPath = moduleDoc.path;
-
-						// // This is kind of a best guess at components. "thing.component.ts"
-						// if (!importPath.endsWith('.component.ts')) {
-						// 	return;
-						// }
-
-						// const tagNameWithoutPrefix = path.basename(importPath, '.component.ts');
-						// const tagName = 'sl-' + tagNameWithoutPrefix;
-
-						// classDoc.tagNameWithoutPrefix = tagNameWithoutPrefix;
-						// classDoc.tagName = tagName;
-
-						// // This used to be set to true by @customElement
-						// classDoc.customElement = true;
 					}
 				}
 			},
@@ -48,15 +29,7 @@ export default {
 
 		customElementVuejsPlugin({
 			outdir: './types',
-			fileName: 'vue.d.ts',
-			// componentTypePath(name, tag) {
-			// 	console.log({ name, tag });
-			// },
-			// componentTypePath: (name, tag) => {
-			// 	console.log({ name, tag });
-			// 	return './src/e-button.ts';
-			// },
-			// globalTypePath: './src/appTypes.ts',
+			fileName: 'web-components.vue.d',
 		}),
 	],
 };
