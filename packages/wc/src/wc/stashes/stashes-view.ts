50d0cb6f01c1e414e2b38fdd6e1aee06a6bafe74
shonya3 <poeshonya3@gmail.com>
2024-06-14 07:31:53 +0300

    use try-finally to reduce available downloads even after error

--- packages/wc/src/wc/stashes/stashes-view.ts
+++ packages/wc/src/wc/stashes/stashes-view.ts
@@ -18,6 +18,7 @@ import '@shoelace-style/shoelace/dist/components/spinner/spinner.js';
 import { isStashTabError } from '@divicards/shared/error';
 import { StashTabErrorsElement } from './e-stash-tab-errors';
 import { ErrorLabel } from './types';
+import { styles } from './stashes-view.styles';
 
 const SECS_300 = 300 * 1000;
 const SECS_10 = 10 * 1000;
@@ -62,7 +63,7 @@ export class StashesViewElement extends BaseElement {
 		return [HelpTipElement, LeagueSelectElement, TabBadgeElement, TabBadgeGroupElement, StashTabErrorsElement];
 	}
 	static override tag = 'wc-stashes-view';
-	static override styles = [styles()];
+	static override styles = styles;
 
 	@property({ reflect: true }) league: League = ACTIVE_LEAGUE;
 	@property() downloadAs: DownloadAs = 'divination-cards-sample';
@@ -283,87 +284,20 @@ export class StashesViewElement extends BaseElement {
 		}
 
 		this.msg = '';
-		const singleTabContent = await loadFunction(id, league);
-
-		this.stashLoadsAvailable--;
-		this.availableInTenSeconds--;
-		setTimeout(() => {
-			this.stashLoadsAvailable++;
-		}, SECS_300);
-		setTimeout(() => {
-			this.availableInTenSeconds++;
-		}, SECS_10);
-		return singleTabContent;
+		try {
+			const singleTabContent = await loadFunction(id, league);
+			return singleTabContent;
+		} finally {
+			this.stashLoadsAvailable--;
+			this.availableInTenSeconds--;
+			setTimeout(() => {
+				this.stashLoadsAvailable++;
+			}, SECS_300);
+			setTimeout(() => {
+				this.availableInTenSeconds++;
+			}, SECS_10);
+		}
 	}
-}
-
-function styles() {
-	return css`
-		:host {
-			display: block;
-			max-width: 1500px;
-		}
-
-		.main-stashes-component {
-			position: relative;
-			padding: 1rem;
-			padding: 0.6rem;
-			border: 0.5px solid rgba(0, 0, 0, 0.6);
-			border-radius: 0.25rem;
-		}
-
-		wc-help-tip::part(tooltip) {
-			right: 5px;
-		}
-
-		.header {
-			display: flex;
-			justify-content: space-between;
-		}
-
-		.tips {
-			display: flex;
-			gap: 0.5rem;
-			margin-top: 0.75rem;
-		}
-
-		.top-right-corner {
-			display: flex;
-			gap: 2rem;
-		}
-
-		.btn-load-items:not([disabled]) {
-			transform: scale(1.2);
-		}
-
-		.messages {
-			position: relative;
-		}
-
-		.msg {
-			position: absolute;
-			font-size: 2rem;
-			max-width: max-content;
-			margin-inline: auto;
-			visibility: hidden;
-			margin-block: 0;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-		}
-
-		.visible {
-			visibility: visible;
-		}
-
-		.not-visible {
-			visibility: hidden;
-		}
-
-		.hidden {
-			display: none;
-		}
-	`;
 }
 
 const sleepSecs = async (secs: number): Promise<void> => {
