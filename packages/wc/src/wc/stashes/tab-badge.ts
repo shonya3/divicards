13e67fad74f426e4202998ae6ac2b0a19fe4f31c
shonya3 <poeshonya3@gmail.com>
2024-06-26 18:55:26 +0300

    tab-badge-group: add as prop to choose between button and checkbox

--- packages/wc/src/wc/stashes/tab-badge.ts
+++ packages/wc/src/wc/stashes/tab-badge.ts
@@ -21,6 +21,7 @@ export class TabBadgeElement extends BaseElement {
 	@property({ type: Boolean, reflect: true }) selected = false;
 	// /** Any valid CSS color */
 	@property({ reflect: true, attribute: 'color' }) color?: string;
+	@property() as: 'button' | 'checkbox' = 'button';
 
 	@state() tabState!: NoItemsTab;
 
@@ -51,16 +52,22 @@ export class TabBadgeElement extends BaseElement {
 			'--tab-index': `' ${this.tab.index} '`,
 		});
 
-		return html`<div class="tab-badge" style=${cssProps}>
-			${this.nameLabel()}
-			<input
-				@change=${this.#onCheckbox}
-				class="checkbox"
-				type="checkbox"
-				.tabId=${this.tab.id}
-				.checked=${this.selected}
-			/>
-		</div>`;
+		if (this.as === 'checkbox') {
+			return html`<div class="tab-badge-as-checkbox" style=${cssProps}>
+				${this.nameLabel()}
+				<input
+					@change=${this.#onCheckbox}
+					class="checkbox"
+					type="checkbox"
+					.tabId=${this.tab.id}
+					.checked=${this.selected}
+				/>
+			</div>`;
+		}
+
+		if (this.as === 'button') {
+			return html`<button style=${cssProps} class="tab-badge-as-button">${this.nameLabel()}</button>`;
+		}
 	}
 
 	@query('input') checkbox!: HTMLInputElement;
@@ -75,7 +82,48 @@ export class TabBadgeElement extends BaseElement {
 	}
 
 	static styles = css`
-		.tab-badge {
+		.tab-badge-as-button {
+			background-color: var(--badge-color);
+			width: 5.5rem;
+			height: 2.2rem;
+			border-radius: 0.4rem;
+			border: 1px solid #000;
+			cursor: pointer;
+			position: relative;
+			&:hover {
+			}
+			.name {
+				pointer-events: none;
+			}
+
+			&::after {
+				display: block;
+				position: absolute;
+				bottom: 0;
+				right: 0;
+				background-color: rgba(255, 255, 255, 0.06);
+				color: #000;
+				content: var(--tab-index);
+				text-align: center;
+				border-top-left-radius: 2rem;
+				font-size: 0.6rem;
+				min-width: 1rem;
+			}
+		}
+
+		.name {
+			color: var(--badge-color);
+			font-size: 0.85rem;
+			color: #000;
+			position: relative;
+
+			.remove-only {
+				font-size: 60%;
+				vertical-align: sub;
+			}
+		}
+
+		.tab-badge-as-checkbox {
 			width: 5.5rem;
 			height: 2.2rem;
 			aspect-ratio: 1;
@@ -90,18 +138,6 @@ export class TabBadgeElement extends BaseElement {
 			&:has(.checkbox:checked) {
 				transform: scale(1.3);
 				z-index: 2;
-			}
-
-			.name {
-				color: var(--badge-color);
-				font-size: 0.85rem;
-				color: #000;
-				position: relative;
-
-				.remove-only {
-					font-size: 60%;
-					vertical-align: sub;
-				}
 			}
 
 			.checkbox {
