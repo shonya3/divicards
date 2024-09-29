6a8a8edb015be939db49341f7b981ece6f56c83b
shonya3 <poeshonya3@gmail.com>
2024-09-29 21:20:33 +0300

    cleanup

--- packages/wc/src/wc/e-poe-auth.ts
+++ packages/wc/src/wc/e-poe-auth.ts
@@ -3,30 +3,23 @@ import { customElement, property } from 'lit/decorators.js';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
 import { emit } from '../utils';
 
-declare global {
-	interface HTMLElementTagNameMap {
-		'e-poe-auth': PoeAuthElement;
-	}
-}
-
 @customElement('e-poe-auth')
 export class PoeAuthElement extends LitElement {
 	static override styles = [styles()];
-
 	@property({ reflect: true }) name: string = '';
 	@property({ type: Boolean, reflect: true }) loggedIn: boolean = false;
+
 	protected override render() {
-		const logoutButton = html`<sl-button @click=${this.#emitLogout}>Logout</sl-button>`;
-		const loginButton = html`<sl-button @click=${this.#emitLogin}>Login</sl-button>`;
-
-		const template = this.loggedIn
-			? html`<div class="logged-in">
-					<p>${this.name}</p>
-					${logoutButton}
-				</div>`
-			: html`<div>${loginButton}</div>`;
-
-		return html`<div class="poe-auth">${template}</div>`;
+		return html`<div class="poe-auth">
+			${this.loggedIn
+				? html`<div class="logged-in">
+						<p>${this.name}</p>
+						<sl-button @click=${this.#emitLogout}>Logout</sl-button>
+					</div>`
+				: html`<div>
+						<sl-button @click=${this.#emitLogin}>Login</sl-button>
+					</div>`}
+		</div>`;
 	}
 
 	#emitLogin() {
@@ -40,19 +33,17 @@ export class PoeAuthElement extends LitElement {
 
 function styles() {
 	return css`
-		.poe-auth {
-			position: relative;
-		}
 		.logged-in {
 			display: flex;
 			align-items: center;
 			justify-self: center;
 			gap: 1rem;
 		}
-
-		.logs {
-			opacity: 0.6;
-			font-size: 80%;
-		}
 	`;
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'e-poe-auth': PoeAuthElement;
+	}
 }
