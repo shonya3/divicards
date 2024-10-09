0f9418c3d1116bd568e5c2b85ab80fe85f4b9c73
shonya3 <poeshonya3@gmail.com>
2024-10-09 23:27:33 +0300

    deps

--- packages/app/src/elements.d.ts
+++ packages/app/src/elements.d.ts
@@ -1,5 +1,8 @@
 import type { DefineComponent } from 'vue';
 import type { MyEvent, MyCustomEvent } from './events';
+import { MinimumCardsPriceChangeEvent } from '@divicards/wc/src/wc/e-sample-card/e-sample-card';
+import { TradeLeague } from '@divicards/shared/types';
+import { DivinationCardsSample } from '@divicards/shared/types';
 
 declare module 'vue' {
 	// Extending Vue's GlobalComponents interface
@@ -9,19 +12,36 @@ declare module 'vue' {
 interface CustomElements {
 	'my-element': DefineComponent<{
 		name: string;
-		onMyEvent: (e: MyEvent) => void;
-		onMyCustomEvent: (e: MyCustomEvent) => void;
+		onMyEvent?: (e: MyEvent) => void;
+		onMyCustomEvent?: (e: MyCustomEvent) => void;
 	}>;
+	'my-test-sample-card': DefineComponent<
+		{
+			league?: TradeLeague;
+			filename: string;
+			selected: boolean | null;
+			uuid: string;
+			minimumCardPrice: number;
+			sample: DivinationCardsSample;
+		},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{ 'sample__change:minimum_card_price': (e: MinimumCardsPriceChangeEvent) => void }
+	>;
 }
 
 // For JSX/TSX support, extend the IntrinsicElements
-declare global {
-	namespace JSX {
-		interface IntrinsicElements {
-			'my-element': {
-				/** Name for element */
-				name: string;
-			};
-		}
-	}
-}
+// declare global {
+// 	namespace JSX {
+// 		interface IntrinsicElements {
+// 			'my-element': {
+// 				/** Name for element */
+// 				name: string;
+// 			};
+// 		}
+// 	}
+// }
--- packages/wc/custom-elements-manifest.config.js
+++ packages/wc/custom-elements-manifest.config.js
@@ -1,14 +1,30 @@
 // @ts-check
 import nodePath from 'node:path';
 import { customElementVuejsPlugin } from 'custom-element-vuejs-integration';
+/** @import {Plugin} from '@custom-elements-manifest/analyzer' */
+
+/**
+ * @type {Array<Plugin>}
+ */
+const plugins = [
+	{
+		name: 'my-custom-plugin',
+		analyzePhase({ ts, node, moduleDoc, context }) {},
+		collectPhase(params) {
+			console.log(params.context);
+		},
+	},
+];
 
 export default {
-	watch: true,
+	// watch: true,
+	globs: ['src/wc/e-sample-card/e-sample-card.ts'],
 	litelement: true,
 	plugins: [
 		customElementVuejsPlugin({
 			outdir: nodePath.join(import.meta.dirname, 'types'),
 			fileName: 'web-components.vue.d.ts',
 		}),
+		...plugins,
 	],
 };
