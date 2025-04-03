452784f9442d3d46a76044f0906edf1f2ca16502
shonya3 <poeshonya3@gmail.com>
2025-04-03 18:54:03 +0300

    fetch spreadsheet and parse in one go

--- divcord_wasm/src/lib.rs
+++ divcord_wasm/src/lib.rs
@@ -1,26 +1,30 @@
 mod utils;
 
-use divcord::{ParseRecordError, Record, Spreadsheet};
+use divcord::{ParseRecordError, Record};
 use poe_data::PoeData;
 use utils::set_panic_hook;
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
+// Fetch spreadsheet and parse.
 #[wasm_bindgen]
-pub fn parsed_records(
-    spreadsheet: String,
-    poe_data: String,
+pub async fn fetch_divcord_records(
+    poe_data: JsValue,
     on_error: &js_sys::Function,
 ) -> Result<JsValue, JsValue> {
     set_panic_hook();
+    let api_key = env!("GOOGLE_API_KEY");
+
+    let poe_data: PoeData =
+        serde_wasm_bindgen::from_value(poe_data).map_err(|err| JsValue::from(err.to_string()))?;
+    let spreadsheet = divcord::spreadsheet::fetch_spreadsheet(api_key)
+        .await
+        .map_err(|e| JsValue::from_str(&e.to_string()))?;
 
     let on_err = |s: &str| {
         on_error
             .call1(&JsValue::null(), &JsValue::from_str(s))
             .unwrap()
     };
-
-    let spreadsheet: Spreadsheet = serde_json::from_str(&spreadsheet).unwrap();
-    let poe_data: PoeData = serde_json::from_str(&poe_data).unwrap();
 
     let mut records: Vec<Record> = vec![];
     for record in divcord::records_iter(&spreadsheet, &poe_data) {
@@ -34,7 +38,7 @@ pub fn parsed_records(
                 }
             }
             Err(err) => {
-                on_err(err.to_string().as_str());
+                on_err(&err.to_string());
             }
         }
     }
@@ -48,32 +52,22 @@ extern "C" {
     fn log(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn find_cards_by_source_types(types: JsValue, records: JsValue, poe_data: String) -> JsValue {
-    set_panic_hook();
-    let poe_data: PoeData = serde_json::from_str(&poe_data).unwrap();
+// #[wasm_bindgen]
+// pub fn find_cards_by_source_types(types: JsValue, records: JsValue, poe_data: String) -> JsValue {
+//     set_panic_hook();
+//     let poe_data: PoeData = serde_json::from_str(&poe_data).unwrap();
 
-    let types: Vec<String> = serde_wasm_bindgen::from_value(types).unwrap();
-    let records: Vec<Record> = serde_wasm_bindgen::from_value(records).unwrap();
+//     let types: Vec<String> = serde_wasm_bindgen::from_value(types).unwrap();
+//     let records: Vec<Record> = serde_wasm_bindgen::from_value(records).unwrap();
 
-    let cards = divcord::cards::cards_by_source_types(&types, &records, &poe_data);
+//     let cards = divcord::cards::cards_by_source_types(&types, &records, &poe_data);
 
-    log(&format!("{}", cards.len()));
+//     log(&format!("{}", cards.len()));
 
-    serde_wasm_bindgen::to_value(&cards).unwrap()
-}
+//     serde_wasm_bindgen::to_value(&cards).unwrap()
+// }
 
 #[wasm_bindgen]
 pub fn slug(s: String) -> String {
     slug::slugify(s)
-}
-
-#[wasm_bindgen]
-pub async fn fetch_spreadsheet() -> Result<JsValue, JsValue> {
-    let api_key = env!("GOOGLE_API_KEY");
-
-    match divcord::spreadsheet::fetch_spreadsheet(api_key).await {
-        Ok(spreadsheet) => Ok(serde_wasm_bindgen::to_value(&spreadsheet)?),
-        Err(e) => Err(JsValue::from_str(&e.to_string())),
-    }
 }
