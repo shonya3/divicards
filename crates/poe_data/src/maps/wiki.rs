277adafafa41a07daaf173cf58ad5b62b567e154
shonya3 <poeshonya3@gmail.com>
2025-11-30 19:29:21 +0300

    extract queryparams from string

--- crates/poe_data/src/maps/wiki.rs
+++ crates/poe_data/src/maps/wiki.rs
@@ -26,9 +26,27 @@ pub async fn fetch_wiki_maplist() -> Result<Vec<MapDataFromWiki>, reqwest::Error
         pub tier: String,
     }
 
-    let url = format!("{WIKI_API_URL}?action=cargoquery&format=json&smaxage=0&maxage=0&limit=500&tables=maps,items,areas&join_on=items._pageID=maps._pageID,maps.area_id=areas.id&fields=maps.tier,items.name,maps.area_id,maps.area_level,areas.boss_monster_ids,maps.unique_area_id&group_by=items.name&where=items.class_id='Map' AND maps.area_id LIKE '%MapWorlds%'");
+    let params = [
+        ("action", "cargoquery"),
+        ("format", "json"),
+        ("smaxage", "0"),
+        ("maxage", "0"),
+        ("limit", "500"),
+        ("tables", "maps,items,areas"),
+        ("join_on", "items._pageID=maps._pageID,maps.area_id=areas.id"),
+        ("fields", "maps.tier,items.name,maps.area_id,maps.area_level,areas.boss_monster_ids,maps.unique_area_id"),
+        ("group_by", "items.name"),
+        ("where", "items.class_id='Map' AND maps.area_id LIKE '%MapWorlds%'"),
+    ];
 
-    let response: WikiResponse = reqwest::get(url).await?.error_for_status()?.json().await?;
+    let response: WikiResponse = reqwest::Client::new()
+        .get(WIKI_API_URL)
+        .query(&params)
+        .send()
+        .await?
+        .error_for_status()?
+        .json()
+        .await?;
 
     Ok(response
         .cargoquery
