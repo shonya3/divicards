e5c8d476943059538e0492c04c215ad06de485d0
shonya3 <poeshonya3@gmail.com>
2026-04-07 15:47:45 +0300

    Update <e-tab-badge> test with vitest

--- packages/wc/src/wc/stashes/e-tab-badge/e-tab-badge.test.ts
+++ packages/wc/src/wc/stashes/e-tab-badge/e-tab-badge.test.ts
@@ -1,31 +1,69 @@
-import { fixture, expect, html } from "@open-wc/testing";
-import sinon from "sinon";
-import { TabBadgeElement } from "./e-tab-badge.js";
+import { test, expect, vi, describe, beforeEach } from "vitest";
 import "./e-tab-badge.js";
+import type { TabBadgeElement } from "./e-tab-badge.js";
+import { TabClickEvent, TabSelectEvent } from "./events.js";
 
-describe("e-tab-badge", () => {
+describe("<e-tab-badge>", () => {
   let el: TabBadgeElement;
 
   beforeEach(async () => {
-    el = await fixture(html`<e-tab-badge></e-tab-badge>`);
-  });
-
-  it("should render a component", () => {
-    expect(el).to.exist;
-  });
-
-  it("should emit tab-select", async () => {
-    const tabSelectSpy = sinon.spy();
-    el.addEventListener("tab-select", tabSelectSpy);
-    el.selected = false;
-    el.checkbox.click();
+    el = document.createElement("e-tab-badge");
+    el.tab = {
+      id: "Test id",
+      name: "Heist",
+      type: "PremiumStash",
+      index: 0,
+      metadata: { colour: "ff" },
+    };
+    document.body.append(el);
     await el.updateComplete;
 
-    expect(tabSelectSpy).to.have.been.calledOnce;
-    expect(tabSelectSpy.args[0][0].detail).to.deep.equal({
-      tabId: "Test id",
-      selected: true,
-      name: "Heist",
+    return () => {
+      el.remove();
+    };
+  });
+
+  test("should render a component", () => {
+    expect(document.querySelector("e-tab-badge")).to.not.be.null;
+  });
+
+  test("should emit stashes__tab-click on element click when as=button", async () => {
+    const spy = vi.fn();
+
+    el.addEventListener("stashes__tab-click", (e: Event) => {
+      expect(e instanceof TabClickEvent).toBeTruthy();
+      spy();
     });
+
+    el.click();
+
+    expect(spy).toHaveBeenCalledOnce();
+  });
+
+  test("should NOT emit stashes__tab-click on element click when as=checkbox", async () => {
+    el.as = "checkbox";
+    await el.updateComplete;
+
+    const clickHandler = vi.fn();
+    el.addEventListener("stashes__tab-click", clickHandler);
+
+    el.click();
+
+    expect(clickHandler).not.toHaveBeenCalled();
+  });
+
+  test("should emit stashes__tab-select on checkbox change", async () => {
+    el.as = "checkbox";
+    await el.updateComplete;
+
+    const spy = vi.fn();
+    el.addEventListener("stashes__tab-select", (e) => {
+      expect(e instanceof TabSelectEvent).toBeTruthy();
+      spy();
+    });
+
+    el.checkbox.click();
+
+    expect(spy).toHaveBeenCalledOnce();
   });
 });
