ef888ced01d8e4efe90244094352e3ca73b93a38
shonya3 <poeshonya3@gmail.com>
2026-07-24 07:37:05 +0300

    dead api

--- crates/ninja/src/lib.rs
+++ crates/ninja/src/lib.rs
@@ -2,30 +2,7 @@ pub mod card;
 pub mod error;
 
 pub use crate::{
-    card::{fetch_card_data, CardData},
+    card::{fetch_card_data, fetch_exchange_prices, CardData, ExchangeCardPrice},
     error::Error,
 };
 pub use poe::TradeLeague;
-
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-
-pub async fn fetch_by_item_category(
-    item_category: &str,
-    league: &TradeLeague,
-) -> Result<Vec<Value>, Error> {
-    #[derive(Deserialize, Debug, Serialize)]
-    struct ResponseShape {
-        lines: Vec<Value>,
-    }
-
-    let url = format!(
-        "https://poe.ninja/api/data/itemoverview?league={league}&type={item_category}&language=en"
-    );
-    let json = reqwest::get(url).await?.text().await?;
-    let data = serde_json::from_str::<ResponseShape>(&json)?;
-    if data.lines.is_empty() {
-        return Err(Error::NoItemsBadRequest);
-    }
-    Ok(data.lines)
-}
